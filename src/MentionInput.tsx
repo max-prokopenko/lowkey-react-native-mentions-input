51057b421bbb1edabebc3938005ec5e7c722fb9a
Evgenii Usov <evgenusov96@gmail.com>
2021-10-06 11:08:07 +0300

    refactor: rewrite on hooks

---
+++ src/MentionInput.tsx
@@ -0,0 +1,367 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  NativeSyntheticEvent,
+  StyleSheet,
+  TextInput,
+  TextInputKeyPressEventData,
+  TextInputSelectionChangeEventData,
+  TextStyle,
+  View,
+  ViewStyle,
+} from 'react-native';
+
+import matchAll from 'string.prototype.matchall';
+import { PATTERNS } from './constants';
+
+interface Props {
+  value: string;
+  placeholder?: string;
+  placeholderTextColor?: string;
+  multiline?: boolean;
+  onTextChange: (text: string) => void;
+  onMarkdownChange: (markdown: string) => void;
+  onFocusStateChange?: (status: boolean) => void;
+  leftComponent?: React.ReactNode;
+  rightComponent?: React.ReactNode;
+  innerComponent?: React.ReactNode;
+  textInputStyle: ViewStyle;
+  textInputTextStyle: TextStyle;
+  mentionStyle: TextStyle;
+  suggestedUsersComponent: any;
+  users: {
+    id: string;
+    name: string;
+    avatar: string;
+  }[];
+}
+
+type SuggestedUsers = {
+  id: string;
+  name: string;
+  avatar: string;
+  startPosition: number;
+};
+
+export const MentionsInput = ({
+  suggestedUsersComponent,
+  textInputStyle,
+  onFocusStateChange = () => {},
+  onTextChange = () => {},
+  onMarkdownChange = () => {},
+  placeholder = 'Write a message...',
+  placeholderTextColor,
+  multiline,
+  textInputTextStyle,
+  leftComponent = <></>,
+  rightComponent = <></>,
+  innerComponent = <></>,
+  users,
+  ...props
+}: Props) => {
+  const textInputRef = useRef();
+  const [isOpen, SetIsOpen] = useState(false);
+  const [suggestedUsers, SetSuggesedUsers] = useState<SuggestedUsers[]>([]);
+  const [matches, SetMatches] = useState<any[]>([]);
+  const [mentions, SetMentions] = useState<any[]>([]);
+  const [currentCursorPosition, SetCurrentCursorPosition] = useState(0);
+  const [text, SetText] = useState(
+    decodeURI(props.value.replace(/%/g, encodeURI('%')))
+  );
+
+  useEffect(() => {
+    if (props.value === '' && (mentions.length > 0 || matches.length > 0)) {
+      SetMatches([]);
+      SetMentions([]);
+      SetCurrentCursorPosition(1);
+    }
+  }, [matches, mentions, props.value]);
+
+  const transformTag = useCallback((value: string) => {
+    return value
+      .replace(/\s+/g, '')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }, []);
+
+  const handleSuggestionsOpen = useCallback(
+    (values: RegExpMatchArray[]) => {
+      let shouldPresentSuggestions = false;
+      let newSuggestedUsers: Array<SuggestedUsers> = [];
+
+      users.map(
+        (user, index) =>
+          (newSuggestedUsers[index] = {
+            ...user,
+            startPosition: 0,
+          })
+      );
+
+      values.map((match) => {
+        if (match === null) {
+          return;
+        }
+        const matchStartPosition = match.index;
+        if (typeof matchStartPosition === 'undefined') {
+          return;
+        }
+        const matchEndPosition = matchStartPosition + match[0].length;
+
+        if (
+          currentCursorPosition > matchStartPosition &&
+          currentCursorPosition <= matchEndPosition
+        ) {
+          shouldPresentSuggestions = true;
+          newSuggestedUsers = newSuggestedUsers
+            .filter((user) =>
+              user.name
+                .toLowerCase()
+                .includes(match[0].substring(1).toLowerCase())
+            )
+            .map((user) => {
+              user.startPosition = matchStartPosition;
+              return user;
+            });
+        }
+      });
+
+      SetIsOpen(shouldPresentSuggestions);
+      SetSuggesedUsers(newSuggestedUsers);
+    },
+    [currentCursorPosition, users]
+  );
+
+  const formatMarkdown = useCallback(
+    (markdown: string) => {
+      let parseHeadIndex = 0;
+      let markdownArray = [];
+
+      if (mentions.length === 0) {
+        markdownArray.push({
+          type: 'text',
+          data: markdown,
+        });
+      }
+      mentions.map((mention, index) => {
+        let match = matches.find((m) => {
+          return (
+            m.index === mention.user.startPosition &&
+            m[0] === `@${mention.user.name}`
+          );
+        });
+        if (typeof match === 'undefined') {
+          return;
+        }
+        markdownArray.push({
+          type: 'text',
+          data: markdown.substring(parseHeadIndex, mention.user.startPosition),
+        });
+        markdownArray.push({
+          type: 'mention',
+          data: `<@${mention.user.name}::${mention.user.id}>`,
+        });
+        parseHeadIndex =
+          mention.user.startPosition + mention.user.name.length + 1;
+
+        if (index === mentions.length - 1) {
+          markdownArray.push({
+            type: 'text',
+            data: markdown.substring(parseHeadIndex, markdown.length),
+          });
+        }
+      });
+
+      markdown = '';
+
+      markdownArray.map((m) => {
+        if (m.type === 'text') {
+          markdown = markdown + encodeURIComponent(m.data);
+        } else if (m.type === 'mention') {
+          markdown = markdown + m.data;
+        }
+      });
+      onMarkdownChange(markdown);
+    },
+    [onMarkdownChange, mentions, matches]
+  );
+
+  const handleDelete = useCallback(
+    ({ nativeEvent }: NativeSyntheticEvent<TextInputKeyPressEventData>) => {
+      if (nativeEvent.key === 'Backspace') {
+        mentions.map((mention, index) => {
+          const matchStartPosition = mention.user.startPosition;
+          const matchEndPosition =
+            matchStartPosition + mention.user.name.length + 1;
+          if (
+            currentCursorPosition > matchStartPosition &&
+            currentCursorPosition <= matchEndPosition
+          ) {
+            const newMentions = mentions;
+            newMentions.splice(index, 1);
+            SetMentions(newMentions);
+          }
+        });
+      }
+    },
+    [mentions, currentCursorPosition]
+  );
+
+  const onSelectionChange = useCallback(
+    ({
+      nativeEvent,
+    }: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
+      if (nativeEvent.selection.start === nativeEvent.selection.end) {
+        SetCurrentCursorPosition(nativeEvent.selection.start);
+        handleSuggestionsOpen(matches);
+      }
+    },
+    [handleSuggestionsOpen, matches]
+  );
+
+  const notifyTextChanged = useCallback(
+    (newText: string) => {
+      onTextChange(newText);
+      formatMarkdown(newText);
+    },
+    [onTextChange, formatMarkdown]
+  );
+
+  const onChangeText = useCallback(
+    (text: string) => {
+      SetText(text);
+      const newMatches = [...matchAll(text, PATTERNS.USERNAME_MENTION)];
+      const newMentions = text.length > 0 ? mentions : [];
+
+      SetMentions(newMentions);
+      SetMatches(newMatches);
+
+      newMentions.map(async (mention) => {
+        const matchStartPosition = mention.user.startPosition;
+        if (decodeURI(text).length - text.length > 0) {
+          if (
+            matchStartPosition + (text.length - text.length) >
+              currentCursorPosition &&
+            currentCursorPosition !== text.length
+          ) {
+            mention.user.startPosition =
+              mention.user.startPosition + (text.length - text.length);
+          }
+        } else {
+          if (matchStartPosition >= currentCursorPosition) {
+            mention.user.startPosition =
+              mention.user.startPosition + (text.length - text.length);
+          }
+        }
+        return mention;
+      });
+      SetMentions(newMentions);
+      notifyTextChanged(text);
+    },
+    [mentions, currentCursorPosition, notifyTextChanged]
+  );
+
+  const handleAddMentions = useCallback(
+    (user: {
+      id: number;
+      name: string;
+      avatar: string;
+      startPosition: number;
+    }) => {
+      const startPosition = user.startPosition;
+      const mention = mentions.find(
+        (m) => m.user.startPosition === startPosition
+      );
+      if (typeof mention !== 'undefined') {
+        return;
+      }
+
+      const match = matches.find((m) => m.index === startPosition);
+      let newMentions = mentions;
+      const userName = transformTag(user.name);
+      const newText =
+        text.substring(0, match.index) +
+        `@${userName} ` +
+        text.substring(match.index + match[0].length, text.length);
+      newMentions.push({
+        user: {
+          ...user,
+          name: userName,
+          startPosition: startPosition,
+          test: 1000,
+        },
+      });
+      newMentions.sort((a, b) =>
+        a.user.startPosition > b.user.startPosition
+          ? 1
+          : b.user.startPosition > a.user.startPosition
+          ? -1
+          : 0
+      );
+
+      SetMentions(newMentions);
+      SetIsOpen(false);
+      SetCurrentCursorPosition(match.index + user.name.length + 2);
+      onChangeText(newText);
+    },
+    [mentions, matches, onChangeText, transformTag, text]
+  );
+
+  const onFocus = useCallback(() => {
+    onFocusStateChange(true);
+  }, [onFocusStateChange]);
+
+  const onBlur = useCallback(() => {
+    onFocusStateChange(false);
+  }, [onFocusStateChange]);
+
+  useEffect(() => {
+    handleSuggestionsOpen(matches);
+  }, [currentCursorPosition, matches, handleSuggestionsOpen]);
+
+  useEffect(() => {
+    SetText(props.value);
+    notifyTextChanged(props.value);
+  }, [props.value, notifyTextChanged]);
+
+  return (
+    <View>
+      <View>
+        {isOpen && suggestedUsersComponent(suggestedUsers, handleAddMentions)}
+        <View style={styles.inputContainerRow}>
+          <View>{leftComponent}</View>
+          <View style={[textInputStyle, styles.row]}>
+            <TextInput
+              ref={textInputRef.current}
+              onFocus={onFocus}
+              onBlur={onBlur}
+              placeholder={placeholder}
+              placeholderTextColor={placeholderTextColor}
+              multiline={multiline}
+              value={text}
+              onChangeText={onChangeText}
+              onKeyPress={handleDelete}
+              style={[
+                textInputTextStyle,
+                styles.flex,
+                { paddingBottom: multiline ? 5 : 0 },
+              ]}
+              onSelectionChange={onSelectionChange}
+            />
+            {innerComponent}
+          </View>
+          {rightComponent}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  flex: { flex: 1 },
+  row: { flexDirection: 'row' },
+  inputContainerRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    minHeight: 36,
+  },
+});
---
+++ src/utils.tsx
@@ -0,0 +1,166 @@
+import React from 'react';
+import { Text, TextStyle, TouchableWithoutFeedback } from 'react-native';
+import matchAll from 'string.prototype.matchall';
+import { PATTERNS } from './constants';
+
+const decodeURIComponentSafely = (uri: string) => {
+  try {
+    return decodeURIComponent(uri);
+  } catch (e) {
+    console.log('URI Component not decodable: ' + uri);
+    return uri;
+  }
+};
+
+export const parseMarkdown = (
+  text: string,
+  mentionStyle: TextStyle,
+  urlStyle?: TextStyle,
+  handleURL?: (url: string) => void
+) => {
+  let textToParse = text;
+  let parsedTextArray: Array<React.ReactNode> = [];
+  let parseHeadIndex = 0;
+  let matches = [...matchAll(text, PATTERNS.MENTION)];
+
+  if (matches.length === 0) {
+    let currentParsable = decodeURIComponentSafely(textToParse);
+    let matchesURL = [...matchAll(currentParsable, PATTERNS.URL)];
+    if (matchesURL.length > 0) {
+      parsedTextArray.push(
+        <Text>{currentParsable.substring(0, matchesURL[0].index)}</Text>
+      );
+      matchesURL.map((url, index) => {
+        let urlIndex = 0;
+        if (typeof url.index !== 'undefined') {
+          urlIndex = url.index;
+        }
+        parsedTextArray.push(
+          <TouchableWithoutFeedback
+            onPress={() => (handleURL ? handleURL(url[0]) : null)}
+          >
+            <Text style={urlStyle}>
+              {currentParsable.substring(urlIndex, url[0].length + urlIndex)}
+            </Text>
+          </TouchableWithoutFeedback>
+        );
+        parsedTextArray.push(
+          <Text>
+            {currentParsable.substring(
+              url[0].length +
+                (typeof url.index !== 'undefined' ? url.index : 0),
+              index === matchesURL.length - 1
+                ? currentParsable.length
+                : matchesURL[index + 1].index
+            )}
+          </Text>
+        );
+      });
+    } else {
+      return <Text>{decodeURIComponentSafely(textToParse)}</Text>;
+    }
+    return parsedTextArray;
+  }
+  matches.map((match, index) => {
+    const matchedUser = match[0].slice(2, -1);
+    const mention = {
+      id: matchedUser.split('::')[1],
+      name: matchedUser.split('::')[0],
+    };
+
+    let currentParsable = decodeURIComponentSafely(
+      textToParse.substring(parseHeadIndex, match.index)
+    );
+    let matchesURL = [...matchAll(currentParsable, PATTERNS.URL)];
+    if (matchesURL.length > 0) {
+      parsedTextArray.push(
+        <Text>{currentParsable.substring(0, matchesURL[0].index)}</Text>
+      );
+      matchesURL.map((url, index) => {
+        let urlIndex = 0;
+        if (typeof url.index !== 'undefined') {
+          urlIndex = url.index;
+        }
+        parsedTextArray.push(
+          <TouchableWithoutFeedback
+            onPress={() => (handleURL ? handleURL(url[0]) : null)}
+          >
+            <Text style={urlStyle}>
+              {currentParsable.substring(urlIndex, url[0].length + urlIndex)}
+            </Text>
+          </TouchableWithoutFeedback>
+        );
+        parsedTextArray.push(
+          <Text>
+            {currentParsable.substring(
+              url[0].length + urlIndex,
+              index === matchesURL.length - 1
+                ? currentParsable.length
+                : matchesURL[index + 1].index
+            )}
+          </Text>
+        );
+      });
+    } else {
+      parsedTextArray.push(
+        <Text>
+          {decodeURIComponentSafely(
+            textToParse.substring(parseHeadIndex, match.index)
+          )}
+        </Text>
+      );
+    }
+    parsedTextArray.push(
+      <Text style={mentionStyle}>{`@${decodeURI(mention.name)}`}</Text>
+    );
+    if (typeof match.index === 'number') {
+      parseHeadIndex = match.index + match[0].length;
+    }
+
+    if (index === matches.length - 1) {
+      let lastParsable = decodeURIComponentSafely(
+        textToParse.substring(parseHeadIndex, textToParse.length)
+      );
+      let matchesURL = [...matchAll(lastParsable, PATTERNS.URL)];
+      if (matchesURL.length > 0) {
+        parsedTextArray.push(
+          <Text>{lastParsable.substring(0, matchesURL[0].index)}</Text>
+        );
+        matchesURL.map((url, index) => {
+          let urlIndex = 0;
+          if (typeof url.index !== 'undefined') {
+            urlIndex = url.index;
+          }
+          parsedTextArray.push(
+            <TouchableWithoutFeedback
+              onPress={() => (handleURL ? handleURL(url[0]) : null)}
+            >
+              <Text style={urlStyle}>
+                {lastParsable.substring(urlIndex, url[0].length + urlIndex)}
+              </Text>
+            </TouchableWithoutFeedback>
+          );
+          parsedTextArray.push(
+            <Text>
+              {lastParsable.substring(
+                url[0].length + urlIndex,
+                index === matchesURL.length - 1
+                  ? lastParsable.length
+                  : matchesURL[index + 1].index
+              )}
+            </Text>
+          );
+        });
+      } else {
+        parsedTextArray.push(
+          <Text>
+            {decodeURIComponentSafely(
+              textToParse.substring(parseHeadIndex, textToParse.length)
+            )}
+          </Text>
+        );
+      }
+    }
+  });
+  return parsedTextArray;
+};
